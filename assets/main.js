8cf399d2d19f48e33a4c609041056f669d6cdb90
黃詩涵 <liv@huangshihandeMacBook-Pro.local>
2023-02-19 16:29:00 +0800

    Fetch data from backend instead of frontend

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,3 @@
-const consonantData = [['ก', 'chicken', 'M'], ['ข', 'egg', 'H'], ['ฃ', 'bottle', 'H'], ['ค', 'buffalo', 'L'], ['ฅ', 'person', 'L'], ['ฆ', 'bell', 'L'], ['ง', 'snake', 'L'], ['จ', 'plate_dish', 'M'], ['ฉ', 'cymbal', 'H'], ['ช', 'elephant', 'L'], ['ซ', 'chain', 'L'], ['ฌ', 'tree', 'L'], ['ญ', 'woman', 'L'], ['ฎ', 'headdress', 'M'], ['ฏ', 'goad', 'M'], ['ฐ', 'base', 'H'], ['ฑ', 'actress_name', 'L'], ['ฒ', 'old_person', 'L'], ['ณ', 'novice monk', 'L'], ['ด', 'child', 'M'], ['ต', 'turtle', 'M'], ['ถ', 'bag_sack', 'H'], ['ท', 'solider', 'L'], ['ธ', 'flag', 'L'], ['น', 'mouse', 'L'], ['บ', 'leaf', 'M'], ['ป', 'fish', 'M'], ['ผ', 'bee', 'H'], ['ฝ', 'lid', 'H'], ['พ', 'kind_of_tray', 'L'], ['ฟ', 'tooth_teeth', 'L'], ['ภ', 'sailboat', 'L'], ['ม', 'horse', 'L'], ['ย', 'ogre', 'L'], ['ร', 'boat', 'L'], ['ล', 'monkey', 'L'], ['ว', 'ring', 'L'], ['ศ', 'pavilion', 'H'], ['ษ', 'hermit', 'H'], ['ส', 'tiger', 'H'], ['ห', 'chest', 'H'], ['ฬ', 'kind_of_kite', 'L'], ['อ', 'basin', 'S'], ['ฮ', 'owl', 'L']];
-
-const vowelData = [['อะ', 'a', '[a]'], ['อา', 'aa', '[aː]'], ['อิ', 'i', '[i]'], ['อี', 'ii', '[iː]'], ['อึ', 'ue', '[ɯ]'], ['อื', 'uee', '[ɯː]'], ['อุ', 'u', '[u]'], ['อู', 'uu', '[uː]'], ['เอะ', 'e', '[e]'], ['เอ', 'ay', '[eː]'], ['เเอะ', 'ae’', '[ɛ]'], ['เเอ', 'ae', '[ɛː]'], ['โอะ', 'o', '[o]'], ['โอ', 'oh', '[oː]'], ['เอาะ', 'or’', '[ɔ]'], ['ออ', 'or', '[ɔː]'], ['เออะ', 'oe’', '[ɤ]'], ['เออ', 'oe', '[ɤː]'], ['เอียะ', 'ia’', '[ia]'], ['เอีย', 'ia', '[iaː]'], ['เอือะ', 'uea’', '[ɯa]'], ['เอือ', 'uea', '[ɯaː]'], ['อัวะ', 'ua’', '[ua]'], ['อัว', 'ua', '[uaː]'], ['อ ํา', 'am', '[am]'], ['เอา', 'ao', '[ao]'], ['ไอ', 'ai', '[ai]'], ['ใอ', 'ai', '[ai]'], ['ไอย', 'ai', '[ai]'], ['อัย', 'ai', '[ai]'], ['ฤ', 'rue', '[rue]'], ['ฤๅ', 'ruee', '[rueː]'], ['ฦ', 'lue', '[lue]'], ['ฦๅ', 'luee', '[lueː]']];
-
-
 class Thai {
   constructor() {
     this.initList = [];
@@ -10,6 +5,9 @@ class Thai {
     this.randomList = [];
     this.topicType = 'consonant';
     this.quizTypeIndex = 0;
+    this.consonantData = [];
+    this.vowelData = [];
+    this.alreadyGetData = false;
   }
   // Pick a random word from the list but not the same word twice
   pickWord() {
@@ -39,21 +37,34 @@ class Thai {
     return this.randomList;
   }
 
-  init() {
+  // Call server /api/quiz to get quiz data
+  async getQuizData() {
+    const response = await fetch('/api/quiz');
+    const data = await response.json();
+    this.consonantData = data.consonantData;
+    this.vowelData = data.vowelData;
+  };
+
+  async init() {
     const questionNum = parseInt(document.getElementById('select-question-num').value);
     const topicType = this.getTopicType();
+    if (!this.alreadyGetData) {
+      await this.getQuizData();
+      this.alreadyGetData = true;
+    }
+
     let data;
     this.restart('init');
     // Refer to consonantData or vowelData
     switch (topicType) {
       case 'consonant':
-        data = consonantData;
+        data = this.consonantData;
         break;
       case 'vowel':
-        data = vowelData;
+        data = this.vowelData;
         break;
       default:
-        data = consonantData;
+        data = this.consonantData;
         break;
     }
     // If questionNum is not 0 and less than data length
--- index.js
+++ index.js
@@ -36,6 +36,18 @@ app.get('/', async (req, res) => {
   }
 });
 
+// Called from front end to get quiz data
+app.get('/api/quiz', async (req, res) => {
+  const consonantData = [['ก', 'chicken', 'M'], ['ข', 'egg', 'H'], ['ฃ', 'bottle', 'H'], ['ค', 'buffalo', 'L'], ['ฅ', 'person', 'L'], ['ฆ', 'bell', 'L'], ['ง', 'snake', 'L'], ['จ', 'plate_dish', 'M'], ['ฉ', 'cymbal', 'H'], ['ช', 'elephant', 'L'], ['ซ', 'chain', 'L'], ['ฌ', 'tree', 'L'], ['ญ', 'woman', 'L'], ['ฎ', 'headdress', 'M'], ['ฏ', 'goad', 'M'], ['ฐ', 'base', 'H'], ['ฑ', 'actress_name', 'L'], ['ฒ', 'old_person', 'L'], ['ณ', 'novice monk', 'L'], ['ด', 'child', 'M'], ['ต', 'turtle', 'M'], ['ถ', 'bag_sack', 'H'], ['ท', 'solider', 'L'], ['ธ', 'flag', 'L'], ['น', 'mouse', 'L'], ['บ', 'leaf', 'M'], ['ป', 'fish', 'M'], ['ผ', 'bee', 'H'], ['ฝ', 'lid', 'H'], ['พ', 'kind_of_tray', 'L'], ['ฟ', 'tooth_teeth', 'L'], ['ภ', 'sailboat', 'L'], ['ม', 'horse', 'L'], ['ย', 'ogre', 'L'], ['ร', 'boat', 'L'], ['ล', 'monkey', 'L'], ['ว', 'ring', 'L'], ['ศ', 'pavilion', 'H'], ['ษ', 'hermit', 'H'], ['ส', 'tiger', 'H'], ['ห', 'chest', 'H'], ['ฬ', 'kind_of_kite', 'L'], ['อ', 'basin', 'S'], ['ฮ', 'owl', 'L']];
+  const vowelData = [['อะ', 'a', '[a]'], ['อา', 'aa', '[aː]'], ['อิ', 'i', '[i]'], ['อี', 'ii', '[iː]'], ['อึ', 'ue', '[ɯ]'], ['อื', 'uee', '[ɯː]'], ['อุ', 'u', '[u]'], ['อู', 'uu', '[uː]'], ['เอะ', 'e', '[e]'], ['เอ', 'ay', '[eː]'], ['เเอะ', 'ae’', '[ɛ]'], ['เเอ', 'ae', '[ɛː]'], ['โอะ', 'o', '[o]'], ['โอ', 'oh', '[oː]'], ['เอาะ', 'or’', '[ɔ]'], ['ออ', 'or', '[ɔː]'], ['เออะ', 'oe’', '[ɤ]'], ['เออ', 'oe', '[ɤː]'], ['เอียะ', 'ia’', '[ia]'], ['เอีย', 'ia', '[iaː]'], ['เอือะ', 'uea’', '[ɯa]'], ['เอือ', 'uea', '[ɯaː]'], ['อัวะ', 'ua’', '[ua]'], ['อัว', 'ua', '[uaː]'], ['อ ํา', 'am', '[am]'], ['เอา', 'ao', '[ao]'], ['ไอ', 'ai', '[ai]'], ['ใอ', 'ai', '[ai]'], ['ไอย', 'ai', '[ai]'], ['อัย', 'ai', '[ai]'], ['ฤ', 'rue', '[rue]'], ['ฤๅ', 'ruee', '[rueː]'], ['ฦ', 'lue', '[lue]'], ['ฦๅ', 'luee', '[lueː]']];
+
+  let data = {
+    consonantData,
+    vowelData
+  };
+  res.status(200).send(data);
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(
